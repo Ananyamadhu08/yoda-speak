d166dab4859db69e18ae3280b872762747caa494
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-01-11 23:08:00 +0530

    updated app.js using ES6 arrow functions ➡️

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-buttonElement = addEventListener("click", clickHandler)
-
 var textInput = document.querySelector("textarea");
 
 var buttonTranslator = document.querySelector("button");
 
 var textOutput = document.querySelector(".output");
 
-var serverUrl = "https://api.funtranslations.com/translate/yoda.json"
+var serverUrl = "https://api.funtranslations.com/translate/yoda.json";
 
-function getTranslationUrl(input){
-   return serverUrl + "?" + "text=" + input
-}
+const getTranslationUrl = (input) => {
+  return serverUrl + "?" + "text=" + input;
+};
 
-function errorHandler(error){
-    console.log("Error Occured", error.message)
-    alert("Something Went Wrong Please Try Again In Sometime")
-}
+const errorHandler = (error) => {
+  console.log("Error Occured", error.message);
+  alert("Something Went Wrong Please Try Again In Sometime");
+};
 
-function clickHandler(){
-    var result = textInput.value
+const clickHandler = () => {
+  var result = textInput.value;
 
-    fetch(getTranslationUrl(result))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated
-        textOutput.innerHTML = "<h1>" + translatedText + "</h1>"
+  fetch(getTranslationUrl(result))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      textOutput.innerHTML = "<h1>" + translatedText + "</h1>";
     })
-    .catch(errorHandler)
-}
+    .catch(errorHandler);
+};
+
+buttonElement = addEventListener("click", clickHandler);
